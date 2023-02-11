971f71ef55e0712e374e17bb0d5ed43fff4aacb4
Kairos Jeong <kairosys@gmail.com>
2023-02-11 01:56:17 -0800

    Widget Lifecycle: initSatte->Bulid->dispose

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,14 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,7 +33,14 @@ class _MyAppState extends State<MyApp> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [MyLargeTitle()],
+            children: [
+              showTitle ? const MyLargeTitle() : const Text('Nothing'),
+              IconButton(
+                  onPressed: toggleTitle,
+                  icon: const Icon(
+                    Icons.remove_red_eye,
+                  )),
+            ],
           ),
         ),
       ),
@@ -33,20 +48,38 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    print('dispose');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('fine thank');
     return Text(
       'My Large Title',
       style: TextStyle(
         fontSize: 30,
         //BuildContext를 통해서 parent의 theme를 사용가능.
         color: Theme.of(context).textTheme.titleLarge?.color,
-
       ),
     );
   }
